19b59e351a5156d104a377d241f354d007cc22ec
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2022-01-31 15:00:42 -0800

    baseline users API done; need to add password hashing when storing user data

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,7 +1,9 @@
 exports.up = async (knex) => {
   await knex.schema.createTable("users", (users) => {
     users.increments("user_id");
-    users.string("username", 200).notNullable();
+    users.string("username", 200)
+      .notNullable()
+      .unique();
     users.string("password", 200).notNullable();
     users.boolean("is_owner").defaultTo(false);
   });
