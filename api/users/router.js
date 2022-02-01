ede2bd8db29cc629f016f0259542d541f995f333
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2022-02-01 10:43:59 -0800

    added locations endpoint and refactored users code

--- api/users/router.js
+++ api/users/router.js
@@ -3,13 +3,23 @@ const Users = require('./model.js');
 
 const { validateUser, checkUsernameExists } = require('./middleware.js');
 
-router.get('/', async (req, res) => {
-    res.json(await Users.getAllUsers());
+router.get('/', async (req, res, next) => {
+    try {
+        const users = await Users.getAllUsers();
+        res.status(200).json(users);
+    } catch (err) {
+        next(err);
+    }
 })
   
-router.post('/', validateUser, checkUsernameExists, async (req, res) => {
+router.post('/', validateUser, checkUsernameExists, async (req, res, next) => {
     // Need to implement password hashing
-    res.status(201).json(await Users.insertUser(req.body));
+    try {
+        const user = await Users.insertUser(req.body);
+        res.status(201).json(user);
+    } catch (err) {
+        next(err);
+    }
 })
 
 module.exports = router;
